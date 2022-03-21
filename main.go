3e7847146905fb3d6da03481ed781732a6271b6f
Ariel Piñeiro <arielsrv@gmail.com>
2022-03-21 12:49:22 +0100

    routing

--- main.go
+++ main.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"course/src/users/application"
-	"course/src/users/domain"
 	"course/src/users/infrastructure"
-	"fmt"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-	var userRepository domain.UserRepository
-	userRepository = new(infrastructure.HttpUserRepository)
+	router := mux.NewRouter()
+	router.HandleFunc("/user", infrastructure.GetUser)
+	router.HandleFunc("/ping", PingHandler)
+	http.Handle("/", router)
 
-	var userService = application.NewUserService(userRepository)
-	var userDto = userService.GetUser(1)
+	userRepository := new(infrastructure.HttpUserRepository)
+	userService := application.NewUserService(userRepository)
+	infrastructure.NewUserController(userService)
 
-	fmt.Println(userDto.Id)
-	fmt.Println(userDto.Name)
-	fmt.Println(userDto.Email)
+	server := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("Listening...")
+	log.Fatal(server.ListenAndServe())
+}
+
+func PingHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(200)
+	json.NewEncoder(writer).Encode("pong")
 }
---
+++ src/users/infrastructure/UserController.go
@@ -0,0 +1,21 @@
+package infrastructure
+
+import (
+	"course/src/users/application"
+	"course/src/users/domain"
+	"encoding/json"
+	"net/http"
+)
+
+type UserController struct {
+	userService *application.UserService
+}
+
+func NewUserController(userService *application.UserService) *UserController {
+	return &UserController{userService: userService}
+}
+
+func GetUser(writer http.ResponseWriter, request *http.Request) {
+	user := domain.User{Id: 1, Name: "Steve Jobs", Email: "stevejobs@apple.com"}
+	json.NewEncoder(writer).Encode(user)
+}
